6f941699e63d92da428cf7270804716106fa8e24
GnuTVan <dangtung011202@gmail.com>
2025-06-08 19:48:37 +0700

    Initial commit

---
+++ eewms/src/main/java/com/example/eewms/ElectricalEquipmentWarehouseManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.eewms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricalEquipmentWarehouseManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricalEquipmentWarehouseManagementApplication.class, args);
+	}
+
+}
---
+++ eewms/src/test/java/com/example/eewms/ElectricalEquipmentWarehouseManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eewms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElectricalEquipmentWarehouseManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
